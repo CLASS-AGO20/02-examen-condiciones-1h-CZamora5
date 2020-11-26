208f78437c8ca59d7e1f72102abaaef1a9a8b79f
Carlos Zamora <czamora5@ucol.mx>
2020-11-26 15:43:29 -0600

    Added test cases for puedeCircular method

--- src/main.js
+++ src/main.js
@@ -63,3 +63,19 @@ console.log(`Si conduciste por ${kilometros2} kilometros el costo de renta será
 console.log(`Si conduciste por ${2*kilometros2} kilometros el costo de renta será ${app.costoRenta(2*kilometros2)}`);
 console.log(`Si conduciste por ${3*kilometros1} kilometros el costo de renta será ${app.costoRenta(3*kilometros1)}`);
 console.log(`Si conduciste por ${5*kilometros2} kilometros el costo de renta será ${app.costoRenta(5*kilometros2)}`);
+
+//Pruebas del método puedeCircular
+let dia = 1;
+let terminacion = 1;
+console.log(`Probando método puedeCircular(dia, terminacion):`);
+console.log(`¿Puedo circular el día ${dia} con terminación ${0*terminacion}? ${app.puedeCircular(dia, 0*terminacion)}`);
+console.log(`¿Puedo circular el día ${2*dia} con terminación ${1*terminacion}? ${app.puedeCircular(2*dia, terminacion)}`);
+console.log(`¿Puedo circular el día ${3*dia} con terminación ${2*terminacion}? ${app.puedeCircular(3*dia, 2*terminacion)}`);
+console.log(`¿Puedo circular el día ${3*dia} con terminación ${2*terminacion}? ${app.puedeCircular(3*dia, 2*terminacion)}`);
+console.log(`¿Puedo circular el día ${3*dia} con terminación ${1*terminacion}? ${app.puedeCircular(3*dia, terminacion)}`);
+console.log(`¿Puedo circular el día ${4*dia} con terminación ${1*terminacion}? ${app.puedeCircular(4*dia, terminacion)}`);
+console.log(`¿Puedo circular el día ${5*dia} con terminación ${3*terminacion}? ${app.puedeCircular(5*dia, 3*terminacion)}`);
+console.log(`¿Puedo circular el día ${dia} con terminación ${1*terminacion}? ${app.puedeCircular(dia, terminacion)}`);
+console.log(`¿Puedo circular el día ${2*dia} con terminación ${0*terminacion}? ${app.puedeCircular(2*dia, 0*terminacion)}`);
+console.log(`¿Puedo circular el día ${2*dia} con terminación ${7*terminacion}? ${app.puedeCircular(2*dia, 7*terminacion)}`);
+console.log(`¿Puedo circular el día ${9*dia} con terminación ${0*terminacion}? ${app.puedeCircular(9*dia, 0*terminacion)}`);
