893062205296c28c4c2caf088becf352dd38ca7c
Carlos Zamora <czamora5@ucol.mx>
2020-11-26 15:15:04 -0600

    Added test cases for convertir method

--- src/main.js
+++ src/main.js
@@ -8,3 +8,12 @@ export default class App {
 }
 
 let app = new App();
+
+//Pruebas del método convertir
+let pesos1 = 1999;
+let pesos2 = 2500;
+console.log(`Probando método convertir(pesos):`);
+console.log(`${pesos1} pesos equivale a ${app.convertir(pesos1)}`);
+console.log(`${pesos1+1} pesos equivale a ${app.convertir(pesos1+1)}`);
+console.log(`${pesos2} pesos equivale a ${app.convertir(pesos2)}`);
+console.log(`${pesos2/2} pesos equivale a ${app.convertir(pesos2/2)}`);
